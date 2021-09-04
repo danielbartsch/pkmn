d4611f7df44fe082bf7b5dd162c2983fc64a500b
Daniel Bartsch <daniel.bartsch@live.at>
2021-09-04 23:06:09 +0200

    add text wrapping, if text is too long, so it automatically wraps

--- src/animateText.ts
+++ src/animateText.ts
@@ -10,7 +10,11 @@ export const animateText = async (
   options: Partial<Omit<typeof gameState["textAnimation"], "text">> = {}
 ) => {
   options.startedAt = options.startedAt ?? Date.now()
-  gameState.textAnimation = { ...gameState.textAnimation, text, ...options }
+  gameState.textAnimation = {
+    ...gameState.textAnimation,
+    text: wrapText(text, gameState.width),
+    ...options,
+  }
   await sleep(getTextRenderTime(gameState.textAnimation))
   gameState.textAnimation = {
     ...gameState.textAnimation,
@@ -18,3 +22,30 @@ export const animateText = async (
     text: "",
   }
 }
+
+export const wrapText = (text: string, maxWidth: number): string => {
+  if (textLength(text) > maxWidth) {
+    const words = text.split(" ")
+    const { current: wordsUnderMaxWidth, missing } = words.reduce(
+      (acc, word) => {
+        if (textLength(acc.current.join(" ")) + textLength(word) < maxWidth) {
+          acc.current.push(word)
+        } else {
+          acc.missing.push(word)
+        }
+        return acc
+      },
+      { current: [] as Array<string>, missing: [] as Array<string> }
+    )
+    return (
+      wordsUnderMaxWidth.join(" ") +
+      "\n" +
+      wrapText(missing.join(" "), maxWidth)
+    )
+  }
+  return text
+}
+
+// text length without unprinted characters like formatting sequences
+export const textLength = (text: string) =>
+  text.replace(/(\0|\x1b\[[0-9]+m)/g, "").length
---
+++ src/test.ts
@@ -0,0 +1,26 @@
+//@ts-ignore
+import { test, assertEqual, run } from "@danielbartsch/testing"
+import { wrapText, textLength } from "./animateText"
+import * as textFormat from "./textFormat"
+
+test("text below maximum length", () => {
+  const text = "Short text"
+  assertEqual(text, wrapText(text, 20))
+})
+test("text slightly above maximum length", () => {
+  const text = "Short text"
+  assertEqual("Short\ntext", wrapText(text, 9))
+})
+
+test("text length without anything special", () => {
+  assertEqual(5, textLength("hello"))
+})
+test("text length with unprinted characters such as \\0", () => {
+  assertEqual(5, textLength("h\0e\0l\0l\0o\0\0\0\0"))
+})
+test("text length with formatting", () => {
+  assertEqual(5, textLength(textFormat.red("hello")))
+  assertEqual(5, textLength(textFormat.bold("hello")))
+})
+
+run()
